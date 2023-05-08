75ab2d49d5bbe2428a913a8ededc8a43eb2716e5
xOpusMagnumx <zambrano.meza901@gmail.com>
2023-05-08 11:50:47 -0500

    Added spartan font to chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 
 import { ChakraProvider } from '@chakra-ui/react'
-
+import { extendTheme } from '@chakra-ui/react'
+import '@fontsource/spartan/400.css'
 import type { AppProps } from 'next/app'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Spartan', sans-serif`,
+    body: `'Spartan', sans-serif`,
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,30 @@
 import Head from 'next/head'
 import { Button, ButtonGroup, Box, Heading } from '@chakra-ui/react'
 import { useState } from 'react'
+import { GetServerSidePropsContext } from 'next';
 
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    count: number;
+    rate: number;
+  } 
+  }
+
+type Props = {
+  products: Product[]
+}
 //Componente de react, la pagina es todo un componente.
-export default function CompReactexportado() {
+export default function CompReactexportado({products}: Props) {
   //PRUEBA
+  console.log(products);
   const [locadia, setState] = useState('null');
-
+  //render con react es con map, se cogieron los productos de los props exportados directamente
   return (
     <>
       <Head>
@@ -16,14 +34,24 @@ export default function CompReactexportado() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-          <Heading as="h3" size="lg">We got Chakra UI</Heading>
-          <Button size="lg" variant='ghost' onClick={() => {
-            alert('Do not click it')
-          }} colorScheme='blue'>Button</Button>
-          <Box bg='tomato' w='80%' p={4} color='white'>
-            Se creo la caja
-          </Box>
+        <ol>
+        {products.map(product => {
+          return <li key={product.id}>{product.title}</li>
+        })}
+        </ol>
       </main>
     </>
   )
+}
+//esta parte del código se ejecuta en el servidor, se está tratando de obtener el contenido antes de cargar la página.
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+
+  const products = await fetch('https://fakestoreapi.com/products')
+      .then(res=>res.json())
+  
+  return {
+    props: {
+      products
+    }
+  }
 }
