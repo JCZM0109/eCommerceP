f0c1455bab8a394ec39edd2633dbd4811012753e
xOpusMagnumx <zambrano.meza901@gmail.com>
2023-05-30 22:30:11 -0500

    Added images to the newsletter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,13 +13,17 @@ import { GroupedProducts, groupProductsByCategory } from '@/utils/groupProductsB
 import * as React from 'react';
 import { HomeHeroCategories } from '@/components/HomeHeroCategories';
 import { Categories } from '@/models/Categories';
-import { Box, Button, Container, FormControl, FormHelperText, FormLabel, Grid, Heading, Input, SimpleGrid, Text } from '@chakra-ui/react';
+import { Box, Button, Container, Flex, FormControl, FormHelperText, FormLabel, Grid, Heading, Input, SimpleGrid, Text } from '@chakra-ui/react';
 import { AdvantageSection } from '@/components/AdvantageSection';
 import { HomeProductsGrid } from '@/components/HomeProductsGrid';
+import { PromoBanner } from '@/components/PromoBanner';
 
 import bannerSale from '/public/banner-sale.jpg';
 import bannerNewSeason from '/public/banner-new-season.jpg';
-import { PromoBanner } from '@/components/PromoBanner';
+import womenStanding from '/public/woman-standing.png';
+import menWalking from '/public/men-walking.png';
+
+
 
 
 
@@ -118,31 +122,83 @@ export default function Home({ products, categories, productsGroupedByCategory }
           </SimpleGrid>
         </Container>
 
-        <Container background={'linear-gradient( #F3F2F2 0%, #DCDBDB 100%);'} m="2rem auto" p="1.5rem" maxW="100%">
-          <Box maxW="33rem" margin="auto" as="article" bgColor="white" p="2rem" textTransform="uppercase">
-          <Grid gap="2rem" maxW="22rem" m="auto" textAlign="center" >
-            <header>
-              <Heading size="sm" color="gray">
-                Special Offer
-              </Heading>
-              <Heading size="lg">
-                Subscribe <br></br> and <Text as="span" color="red">get 10% off</Text>
-              </Heading>
-            </header>
-            < Grid as="form" action='' gap="1.5rem">
-              <FormControl>
-                <Input
-                  type='email'
-                  borderRadius="0"
-                  textAlign="inherit"
-                  placeholder="Enter your email"
-                  h="4rem"
-                  backgroundColor="gray.100" />
-              </FormControl>
-              <Button bgColor="black" size="lg" h="4rem" borderRadius="0" w="100%">Subscribe</Button>
-            </Grid>
-          </Grid>
+        <Container background={'linear-gradient( #F3F2F2 0%, #DCDBDB 100%);'} m={{
+          base: "14.75rem 0 0",
+          md: "2rem auto",
+        }} p={{
+          base: "1.5rem",
+          md: "3.55rem",
+        }} maxW="100%" position="relative">
+          <Box position={"absolute"} w={{
+            base: "99px",
+            md: "219px",
+          }} h={{
+            base: "236px",
+            md: "524px",
+          }}
+          top={{
+            base: "calc(-236px + 1.5rem)",
+            md: "initial",
+          }}
+          bottom={{
+            md: "0",
+          }}
+          right={{
+            base: "2rem",
+            md: "50%",
+          }}
+          transform={{
+            md: "translateX(470px)",
+          }}>
+            <Image src={menWalking} alt='' fill={true} style={{ objectFit: 'cover'}}/>
           </Box>
+          <Box position={"absolute"} w={{
+            base: "128px",
+            md: "311px",
+          }} h={{
+            base: "242px",
+            md: "545px",
+          }} top={{
+            base: "calc(-242px + 1.5rem)",
+            md: "initial",
+          }} bottom={{
+            md: "0",
+          }} left={{
+            base: "1.5rem",
+            md: "50%",
+          }} transform={{
+            md: "translateX(-530px)",
+          }}>
+            <Image src={womenStanding} alt='' fill={true} style={{ objectFit: 'cover'}} />
+          </Box>
+          <Flex 
+            h={{
+            md: "28.75rem",
+            }} 
+            maxW="33rem" margin="auto" as="article" bgColor="white" p="2rem" textTransform="uppercase">
+            <Grid gap="2rem" maxW="22rem" m="auto" textAlign="center" >
+              <header>
+                <Heading size="sm" color="gray">
+                  Special Offer
+                </Heading>
+                <Heading size="lg">
+                  Subscribe <br></br> and <Text as="span" color="red">get 10% off</Text>
+                </Heading>
+              </header>
+              < Grid as="form" action='' gap="1.5rem">
+                <FormControl>
+                  <Input
+                    type='email'
+                    borderRadius="0"
+                    textAlign="inherit"
+                    placeholder="Enter your email"
+                    h="4rem"
+                    backgroundColor="gray.100" />
+                </FormControl>
+                <Button bgColor="black" size="lg" h="4rem" borderRadius="0" w="100%">Subscribe</Button>
+              </Grid>
+            </Grid>
+          </Flex>
         </Container>
 
 
