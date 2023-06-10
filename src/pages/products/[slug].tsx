c74e50fccd54092ae2dfc37d3bf6152438ef946e
xOpusMagnumx <zambrano.meza901@gmail.com>
2023-06-10 11:59:21 -0500

    Added style to product page

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 import { slugify } from "@/utils/slugify";
 import { Product } from "@/pages";
-import { AspectRatio, Box, Button, Text, Container, Flex, Heading, UnorderedList, ListItem, ListIcon } from "@chakra-ui/react";
+import { AspectRatio, Box, Button, Text, Container, Flex, Heading, UnorderedList, ListItem, ListIcon, SimpleGrid, GridItem, Grid, List, Divider } from "@chakra-ui/react";
 import Link from "next/link";
 import { StarsRating } from "@/components/StarsRating";
 import { ShareIcon } from "@/icons/Share";
@@ -15,19 +15,22 @@ type Props = {
 export default function ProductPage({ product: { id, title, price, rating, category, image, description } }: Props) {
   return (
     <>
-      <Box bg="gray.100" padding="1rem">
+      <Box bg="gray.100" padding="1rem" mb="2rem">
         <Container>
-          <Flex justifyContent="space-between" alignItems="center" fontSize="sm" mb="1rem">
+          <Flex justifyContent="space-between" alignItems="center" fontSize={{
+            base: "2xs",
+            md: "sm",
+            }} mb="1rem">
             <Flex as={UnorderedList} listStyleType="none" m="0">
               <ListItem>
                 <Link href="/">Home</Link>
               </ListItem>
               <ListItem textTransform="capitalize">
-                <ListIcon as={ChevronRightIcon} w="20px" h="20px" mx=".5rem" />
+                <ListIcon as={ChevronRightIcon} w={{base: "15px", md:"20px"}} h={{base: "15px", md:"20px"}} mx={{base: "0.2rem", md: ".5rem"}} />
                 <Link href={`${category}`}>{category}</Link>
               </ListItem>
               <ListItem>
-                <ListIcon as={ChevronRightIcon} w="20px" h="20px" mx=".5rem" />
+                <ListIcon as={ChevronRightIcon} w={{base: "15px", md:"20px"}} h={{base: "15px", md:"20px"}} mx={{base: "0.2rem", md: ".5rem"}} />
                 {title}
               </ListItem>
             </Flex>
@@ -42,24 +45,55 @@ export default function ProductPage({ product: { id, title, price, rating, categ
               <StarsRating rate={rating.rate} />
               <Text>{rating.count} Reviews</Text>
             </Flex>
-
-
             <Flex gap="1.5rem">
-              <Text>SKU: <b>{id}</b></Text>
-              <Text>Availability: <b>In stock</b></Text>
+              <Text fontSize={{
+            base: "2xs",
+            md: "sm",
+            }}>SKU: <b>{id}</b></Text>
+              <Text fontSize={{
+            base: "2xs",
+            md: "sm",
+            }}>Availability: <b>In stock</b></Text>
             </Flex>
           </Flex>
         </Container>
       </Box>
-      <p>{price}</p>
-      <p>{category}</p>
-      <p>{description}</p>
-      <Box w="340px">
-        <AspectRatio position="relative" ratio={1} maxWidth="100%" marginBottom="1rem">
-          <Image src={image} alt={''} fill={true} style={{ objectFit: "contain" }}></Image>
-        </AspectRatio>
-      </Box>
-      <Button>Add to cart</Button>
+      <Container>
+        <Grid gridTemplateColumns={{
+          base: "auto",
+          md: "repeat(2, 1fr)",
+        }} gridTemplateRows={{
+          base: "repeat(2, 1fr)",
+          md: "auto",
+        }} gap="3rem">
+          <GridItem w="25rem">
+              <AspectRatio position="relative" ratio={1} maxWidth="100%" marginBottom="1rem">
+                <Image src={image} alt={''} fill={true} style={{ objectFit: "contain" }}></Image>
+              </AspectRatio>
+          </GridItem>
+          <GridItem>
+            <List>
+            <ListItem>
+                <Heading as="h3" textTransform="uppercase" fontSize="md" mb="1rem">Description</Heading>
+                <Text>{description}</Text>
+              </ListItem>
+              <Divider orientation="horizontal" color="gray.300" borderWidth="1px" m="1.5rem 0" />
+              <ListItem>
+                <Flex gap="3rem" alignItems="center">
+                <Text fontWeight="bold" fontSize="2xl"> ${price}</Text>
+                <Button>Add to cart</Button>
+                </Flex>
+              </ListItem>
+              <Divider orientation="horizontal" color="gray.300" borderWidth="1px" m="1.5rem 0" />
+              {/* <ListItem>
+                
+              </ListItem> */}
+            </List>
+
+          </GridItem>
+        </Grid>
+      </Container>
+
 
     </>
   );
